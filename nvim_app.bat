ec8c106bf0afe2dc3d29141766c8e6dfd29a0095
Fabio <foxy000@duck.com>
2025-12-13 15:59:11 +0100

    Add Windows nvim Installation and Testing

---
+++ nvim_app.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:main
+:: Start nvim if possible
+where nvim >nul 2>&1
+if %errorlevel% == 0 (
+    nvim
+    exit /b
+)
+
+echo NeoVim is not installed, installing NeoVim...
+
+:: Installing Chocolatey if not installed
+where choco >nul 2>&1
+if %errorlevel% == 0 (
+    choco --version
+) else (
+    echo Chocolatey required for installation, installing Chocolatey first
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+)
+
+:: Install NeoVim
+choco install neovim --yes
+nvim --version
+
+:: If --pipeline argument is passed, start nvim and quit immediately
+if "%1" == "--pipeline" (
+    nvim -c "qa"
+) else (
+    nvim
+)
+
+goto :eof
+
